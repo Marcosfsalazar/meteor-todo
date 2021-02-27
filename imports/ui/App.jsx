dff29d562956a56001eee5fc95f534d73fdbbef0
Marcos Salazar <Marcos1010521@gmail.com>
2021-02-27 10:01:34 -0400

    user account sys done

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,32 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '../api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm'
+import { LoginForm } from './LoginForm';
+
+const toggleChecked=( {_id, isChecked } ) => {
+  TasksCollection.update(_id,{
+    $set: {
+      isChecked: !isChecked,
+    }
+  })
+}
+
+const deletTask = ({ _id }) => TasksCollection.remove(_id)
 
 export const App = () => {
-  const [hideCompleted, setHideCompleted] = useState(false);
+  const user = useTracker(() => Meteor.user());
   
-  const hideCompletedFilter = { isChecked: { $ne:true } }
-  const tasks = useTracker(() => 
-    TasksCollection.find(hideCompleted ? hideCompletedFilter : {},{ sort: { createdAt: -1 } })
-    .fetch());
+  const [hideCompleted, setHideCompleted] = useState(false);
 
-  const pendingTasksCount = useTracker(() => 
-    TasksCollection.find(hideCompletedFilter).count()
-  )
+  const hideCompletedFilter = { isChecked: { $ne:true } };
+
+  const userFilter = user ? { userId: user._id } : {};
+
+  const pendingOnlyFilter = {...hideCompletedFilter,...userFilter }
+
+  const tasks = useTracker(() => {
+    if(!user){
+      return [];
+    }
+
+    return TasksCollection.find(
+      hideCompleted ? pendingOnlyFilter : userFilter,
+      {
+        sort: { createdAt: -1 }
+      }
+      ).fetch();
+  });
+
+  const pendingTasksCount = useTracker(() => {
+    if (!user) {
+      return 0;
+    }
+
+    return TasksCollection.find(pendingOnlyFilter).count();
+  });
 
   const pendingTasksTitle = `${ pendingTasksCount ? `(${ pendingTasksCount })` : ''}`;
-  
-  const toggleChecked=( {_id, isChecked } ) => {
-    TasksCollection.update(_id,{
-      $set: {
-        isChecked: !isChecked,
-      }
-    })
-  }
 
-  const deletTask = ({ _id }) => TasksCollection.remove(_id)
+  const logout = () => Meteor.logout();
 
   return(
     <div className='app'>
@@ -34,28 +57,41 @@ export const App = () => {
         <div className="app-bar">
           <div className="app-header">
             <h1>📝️ To Do List</h1>
-            {pendingTasksTitle}
+            { pendingTasksTitle }
           </div>
         </div>
       </header>
 
       <div className="main">
-        <TaskForm />
+        { user ? (
+          <Fragment>
+            <div className="user" onClick={logout}>
+              {user.username} 🚪
+            </div>
+            
+            <TaskForm user={user}/>
 
-        <div className="filter">
-          <button onClick={() => { setHideCompleted(!hideCompleted) }}>
-            {hideCompleted ? 'Show All':'Hide Completed'}
-          </button>
-        </div>
+            <div className="filter">
+              <button onClick={() => { setHideCompleted(!hideCompleted) }}>
+                {hideCompleted ? 'Show All':'Hide Completed'}
+              </button>
+            </div>
 
-        <ul className='tasks'>
-          { tasks.map((task)=> <Task 
-          key={ task._id } task={ task } 
-          onCheckboxClick={toggleChecked} 
-          onDeleteClick={deletTask}
-          />) }
-        </ul>
+            <ul className='tasks'>
+              { tasks.map((task)=> (
+                <Task 
+                  key={ task._id } 
+                  task={ task } 
+                  onCheckboxClick={toggleChecked} 
+                  onDeleteClick={deletTask}
+                />
+              )) }
+            </ul>
+        </Fragment>
+        ) : (
+          <LoginForm />
+        )}
       </div>
     </div>
-  )
+  );
 };
--- server/main.js
+++ server/main.js
@@ -1,13 +1,33 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '../imports/api/TasksCollection'
 
-const insertTask = taskText => TasksCollection.insert({ text: taskText })
+const insertTask = (taskText, user) => 
+  TasksCollection.insert({ 
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(), 
+  })
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
 
 Meteor.startup(() => {
+  if(!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
   // If the Links collection is empty, add some data.
   if (TasksCollection.find().count() === 0) {
     [
-      
-    ].forEach(insertTask)
+      'First Task, do a React Pokedex',
+      'Second Task'
+    ].forEach(taskText => insertTask(taskText,user));
   }
+
 });
