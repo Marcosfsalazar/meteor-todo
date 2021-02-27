474ae67e387dbdc9155d63ff03404b4aac30bd81
Marcos Salazar <Marcos1010521@gmail.com>
2021-02-26 23:17:49 -0400

    fix classname

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -31,7 +31,7 @@ export const App = () => {
       <div className="main">
         <TaskForm />
 
-        <ul>
+        <ul className='tasks'>
           { tasks.map((task)=> <Task 
           key={ task._id } task={ task } 
           onCheckboxClick={toggleChecked} 
